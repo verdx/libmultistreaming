5c14960d8710879f500b4c380f6fea28480bacbc
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-12-04 20:08:17 +0100

    Cambiada la forma de checkear la red

--- DefaultViewModel.java
+++ DefaultViewModel.java
@@ -3,29 +3,37 @@ package d2d.testing.streaming;
 import static android.content.Context.WIFI_SERVICE;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.aware.WifiAwareManager;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.Enumeration;
 
 import d2d.testing.R;
+import d2d.testing.gui.main.WifiAwareNetwork;
 import d2d.testing.streaming.network.DefaultNetwork;
 
 public class DefaultViewModel extends BasicViewModel {
 
-    private DefaultNetwork mNetwork;
+    private final DefaultNetwork mNetwork;
     public static String SERVER_IP = "";
     public static int SERVER_PORT = 8080;
 
@@ -33,33 +41,19 @@ public class DefaultViewModel extends BasicViewModel {
         super(app);
         mNetwork = new DefaultNetwork(app);
         SERVER_IP = getLocalIpAddress();
+
         mIsNetworkAvailable = new MutableLiveData<>(Boolean.TRUE);
 
-        //A partir de aqui estamos creando un callback que actualize automaticamente el estado de red
-        ConnectivityManager connectivityManager = (ConnectivityManager) app.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkRequest networkRequest = new NetworkRequest.Builder()
-                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                .build();
-
-        ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        BroadcastReceiver myReceiver = new BroadcastReceiver() {
             @Override
-            public void onAvailable(@NonNull Network network) {
-                super.onAvailable(network);
-                // Internet está disponible
-                mIsNetworkAvailable.postValue(true);
-            }
-
-            @Override
-            public void onLost(@NonNull Network network) {
-                super.onLost(network);
-                // No hay conexión a internet
-                mIsNetworkAvailable.postValue(false);
+            public void onReceive(Context context, Intent intent) {
+                checkNetworkInterfaceAvailability();
             }
         };
+        app.registerReceiver(myReceiver, intentFilter);
 
-        if (connectivityManager != null) {
-            connectivityManager.registerNetworkCallback(networkRequest, networkCallback);
-        }
+        checkNetworkInterfaceAvailability();
 
     }
 
@@ -93,6 +87,29 @@ public class DefaultViewModel extends BasicViewModel {
         mNetwork.stopLocalServer();
         mNetwork.stopClient();
         super.onCleared();
+    }
+
+    public void checkNetworkInterfaceAvailability() {
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                if (networkInterface.isUp() && !networkInterface.isLoopback()) {
+                    Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                    while (addresses.hasMoreElements()) {
+                        InetAddress address = addresses.nextElement();
+                        // Verifica si la dirección IP es válida y no es de bucle local (loopback)
+                        if (!address.isLoopbackAddress() && !address.isLinkLocalAddress() && !address.isMulticastAddress()) {
+                            mIsNetworkAvailable.postValue(Boolean.TRUE); // Hay una interfaz de red activa que puede acceder a direcciones IP en general
+                            return;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mIsNetworkAvailable.postValue(Boolean.FALSE); // No se encontró una interfaz de red activa para acceder a direcciones IP
     }
 
     public String getLocalIpAddress() {
